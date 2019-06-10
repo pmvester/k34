75d12772f8e666ce24cd7ee6b2ad2486016e0090
Mikael Vester <pmvester@gmail.com>
2019-06-10 07:08:58 +0200

    added try clause to handle unintelligible payloads

--- mqtt2influxdb/mqtt2influxdb.py
+++ mqtt2influxdb/mqtt2influxdb.py
@@ -17,77 +17,80 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     #print(msg.topic)
     #print(str(msg.payload))
-    pl = json.loads(msg.payload)
-    if msg.topic == "k34/bmp085":
-      json_body = [
-        {
-          "measurement": "pressure",
-          "time": pl["time"] * 1000000,
-          "fields": {
-            "pressure": pl["pressure"],
-            "temperature": pl["temperature"]
+    try:
+      pl = json.loads(msg.payload)
+      if msg.topic == "k34/bmp085":
+        json_body = [
+          {
+            "measurement": "pressure",
+            "time": pl["time"] * 1000000,
+            "fields": {
+              "pressure": pl["pressure"],
+              "temperature": pl["temperature"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    elif msg.topic == "k34/heat/water":
-      json_body = [
-        {
-          "measurement": "heatWater",
-          "time": pl["timestamp"] * 1000000,
-          "fields": {
-            "feedTemp": pl["temperature"]["feed"],
-            "returnTemp": pl["temperature"]["return"]
+        ]
+        db.write_points(json_body)
+      elif msg.topic == "k34/heat/water":
+        json_body = [
+          {
+            "measurement": "heatWater",
+            "time": pl["timestamp"] * 1000000,
+            "fields": {
+              "feedTemp": pl["temperature"]["feed"],
+              "returnTemp": pl["temperature"]["return"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    elif msg.topic == "k34/power":
-      json_body = [
-        {
-          "measurement": "power",
-          "time": pl["timestamp"] * 1000000,
-          "fields": {
-            "power": pl["power"]
+        ]
+        db.write_points(json_body)
+      elif msg.topic == "k34/power":
+        json_body = [
+          {
+            "measurement": "power",
+            "time": pl["timestamp"] * 1000000,
+            "fields": {
+              "power": pl["power"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    elif msg.topic == "k34/tempout":
-      json_body = [
-        {
-          "measurement": "tempout",
-          "time": pl["timestamp"] * 1000000,
-          "fields": {
-            "tempout": pl["tempout"]
+        ]
+        db.write_points(json_body)
+      elif msg.topic == "k34/tempout":
+        json_body = [
+          {
+            "measurement": "tempout",
+            "time": pl["timestamp"] * 1000000,
+            "fields": {
+              "tempout": pl["tempout"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    elif msg.topic == "k34/tempgarage":
-      json_body = [
-        {
-          "measurement": "tempgarage",
-          "time": pl["timestamp"] * 1000000,
-          "fields": {
-            "tempout": pl["tempgarage"]
+        ]
+        db.write_points(json_body)
+      elif msg.topic == "k34/tempgarage":
+        json_body = [
+          {
+            "measurement": "tempgarage",
+            "time": pl["timestamp"] * 1000000,
+            "fields": {
+              "tempout": pl["tempgarage"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    elif msg.topic == "k34/heat/burner":
-      json_body = [
-        {
-          "measurement": "oilBurner",
-          "time": pl["timestamp"] * 1000000,
-          "fields": {
-            "burner": pl["burner"]
+        ]
+        db.write_points(json_body)
+      elif msg.topic == "k34/heat/burner":
+        json_body = [
+          {
+            "measurement": "oilBurner",
+            "time": pl["timestamp"] * 1000000,
+            "fields": {
+              "burner": pl["burner"]
+            }
           }
-        }
-      ]
-      db.write_points(json_body)
-    else:
-      pass
+        ]
+        db.write_points(json_body)
+      else:
+        pass
+    except:
+      print("something went wrong, probably unintelligible payload")
 
 db = InfluxDBClient("localhost", 8086, "root", "root", "k34db")
 
@@ -99,6 +102,4 @@ client.connect("localhost", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
